3dce35dec8603921c9389ed1c1303db86449f9a7
GlebShulga <shulga_gleb@hotmail.com>
2022-07-17 17:47:28 +0400

    add error if product was not found

--- shop-info-service/handlers/getProductById.ts
+++ shop-info-service/handlers/getProductById.ts
@@ -3,10 +3,19 @@ import "source-map-support/register";
 import productList from "../src/data/productList.json";
 
 export const getProductById: APIGatewayProxyHandler = async (event) => {
+  const error = 'Product not found'
   const { id } = event?.pathParameters;
-  const product = await productList.find(item => item.id === id)
-  return {
-    statusCode: 200,
-    body: JSON.stringify(product),
-  };
+  const product = await productList.find((item) => item.id === id);
+  const response = product ?? error
+  try {
+    return {
+      statusCode: 200,
+      body: JSON.stringify(response),
+    };
+  } catch (e) {
+    return {
+      statusCode: 404,
+      body: error,
+    };
+  }
 };
